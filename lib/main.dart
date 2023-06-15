9e81deb8b1d08cf8bd2a94fd7fff113465d1f2c7
PedroRozas <p.rozasmateluna@uandresbello.edu>
2023-06-15 14:23:04 -0400

    new screen image to image

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
 import 'package:deciaiv2/home.dart';
+import 'package:deciaiv2/stylebyimage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Deco AI',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: Camara(),
+      home: const Design(),
     );
   }
 }
